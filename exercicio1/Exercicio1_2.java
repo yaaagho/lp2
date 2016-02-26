fd5e87c19efd19dbd271c17c79d26db0e13ffa2e
yaaagho <yagho@outlook.com>
2016-02-26 08:31:08 -0300

    Update Exercicio1_2.java

--- exercicio1/Exercicio1_2.java
+++ exercicio1/Exercicio1_2.java
@@ -1,4 +1,4 @@
-public class Exercicio1_1{
+public class Exercicio1_2 {
   public static void main(String[]args){
     String parametro = args[0];
     System.out.println("O argumento digitado foi: " + parametro)

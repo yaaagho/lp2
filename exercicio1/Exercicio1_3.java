af52d8fd000577dfc15b3859a5841f40189f0260
yaaagho <yagho@outlook.com>
2016-03-02 20:03:22 -0300

    Create Exercicio1_3.java

---
+++ exercicio1/Exercicio1_3.java
@@ -0,0 +1,11 @@
+public class Exercicio1_3{
+  public static void main(String[] args){
+    int a = 40;
+    int b = 20;
+    int c = 3;
+    System.out.println ("O valor da soma foi: " + (a+b+c));
+    System.out.println ("O valor da multiplicacao foi: " + (a*b*c));
+    System.out.println ("O valor da divisao foi: " + (a/b/c));
+    System.out.println ("O valor da subtracao foi " + (a-b-c));
+  }
+}

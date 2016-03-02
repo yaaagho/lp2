856c5c76c3482e260e99ecff15b8320ae550a5b5
yaaagho <yagho@outlook.com>
2016-03-02 20:07:25 -0300

    Create Exercicio1_4.java

---
+++ exercicio1/Exercicio1_4.java
@@ -0,0 +1,11 @@
+public class Exercicio1_4{
+  public static void main(String[] args){
+    float a = 40;
+    float b = 20;
+    float c = 3;
+    System.out.println ("O valor da soma foi: " + (a+b+c));
+    System.out.println ("O valor da multiplicacao foi: " + (a*b*c));
+    System.out.println ("O valor da divisao foi: " + (a/b/c));
+    System.out.println ("O valor da subtracao foi " + (a-b-c));
+  }
+}
